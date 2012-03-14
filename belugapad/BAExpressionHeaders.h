0a7e7f8ac475675cfcd86ec98a63cbd04a39ee0c
Gareth Jenkins <gareth@36peas.com>
2012-03-14 23:50:59 +0000

    added BAExpression files directly (outside of submodule)

---
+++ belugapad/BAExpressionHeaders.h
@@ -0,0 +1,23 @@
+//
+//  BAExpressionHeaders.h
+//  Beluga
+//
+//  Created by Cathy Shive on 1/30/11.
+//  Copyright 2011 Heritage World Press. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "BAExpression.h"
+#import "BAInteger.h"
+#import "BAVariable.h"
+#import "BAOperator.h"
+#import "BAAdditionOperator.h"
+#import "BAMultiplicationOperator.h"
+#import "BADivisionOperator.h"
+#import "BAPowerOperator.h"
+#import "BABracketOperator.h"
+#import "BAEqualsOperator.h"
+#import "BAExpressionBuilder.h"
+#import "BAOperatorSortDescriptor.h"
+
---
+++ belugapad/BAMultiplicationOperator.h
@@ -0,0 +1,15 @@
+//
+//  BAMultiplicationOperator.h
+//  Beluga
+//
+//  Created by Cathy Shive on 1/25/11.
+//  Copyright 2011 Heritage World Press. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BAOperator.h"
+
+@interface BAMultiplicationOperator : BAOperator 
+{
+}
+@end

259a5b88c6413bc4bd73c2636d9653261538a4bc
Gareth Jenkins <gareth@36peas.com>
2012-03-14 00:09:29 +0000

    BATio mathml read parser; mathml consts

---
+++ belugapad/MathMLConsts.m
@@ -0,0 +1,10 @@
+//
+//  MathMLConsts.m
+//  belugapad
+//
+//  Created by Gareth Jenkins on 13/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MathMLConsts.h"
+

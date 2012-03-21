4abe0d9fcc07667e7a08ae2cf9f6ef922c9075bd
Gareth Jenkins <gareth@36peas.com>
2012-03-21 20:00:41 +0000

    commented out pause, start and end calls from toolhost

--- belugapad/ToolHost.m
+++ belugapad/ToolHost.m
@@ -127,8 +127,8 @@
     //do internal mgmt updates
     if(currentTool.ProblemComplete || metaQuestionForceComplete)
     {
-        UsersService *us = ((AppDelegate*)[[UIApplication sharedApplication] delegate]).usersService;
-        [us endProblemAttempt:YES];
+        //UsersService *us = ((AppDelegate*)[[UIApplication sharedApplication] delegate]).usersService;
+        //[us endProblemAttempt:YES];
         
         [self gotoNewProblem];
     }
@@ -265,16 +265,16 @@
         [self.Zubi hideZubi];
     }
     
-    UsersService *us = ((AppDelegate*)[[UIApplication sharedApplication] delegate]).usersService;
-    [us startProblemAttempt];
+    //UsersService *us = ((AppDelegate*)[[UIApplication sharedApplication] delegate]).usersService;
+    //[us startProblemAttempt];
 }
 
 -(void) resetProblem
 {
     skipNextStagedIntroAnim=YES;
     
-    UsersService *us = ((AppDelegate*)[[UIApplication sharedApplication] delegate]).usersService;
-    [us endProblemAttempt:NO];
+    //UsersService *us = ((AppDelegate*)[[UIApplication sharedApplication] delegate]).usersService;
+    //[us endProblemAttempt:NO];
     
     [self loadProblem];
 }
@@ -287,8 +287,8 @@
     [pauseMenu setPosition:ccp(cx, cy)];
     [toolForeLayer addChild:pauseMenu z:10];
     
-    UsersService *us = ((AppDelegate*)[[UIApplication sharedApplication] delegate]).usersService;
-    [us togglePauseProblemAttempt];
+    //UsersService *us = ((AppDelegate*)[[UIApplication sharedApplication] delegate]).usersService;
+    //[us togglePauseProblemAttempt];
 }
 
 -(void) checkPauseTouches:(CGPoint)location
@@ -299,8 +299,8 @@
         [toolForeLayer removeChild:pauseMenu cleanup:YES];
         isPaused=NO;
         
-        UsersService *us = ((AppDelegate*)[[UIApplication sharedApplication] delegate]).usersService;
-        [us togglePauseProblemAttempt];
+        //UsersService *us = ((AppDelegate*)[[UIApplication sharedApplication] delegate]).usersService;
+        //[us togglePauseProblemAttempt];
     }
     if(CGRectContainsPoint(kPauseMenuReset, location))
     {

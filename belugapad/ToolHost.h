88a61b1e3973f4cffef20f75a6fc084a5913fc89
Gareth Jenkins <gareth@36peas.com>
2012-03-21 21:48:56 +0000

    pipeline timer on move to next problem

--- belugapad/ToolHost.h
+++ belugapad/ToolHost.h
@@ -62,6 +62,9 @@ typedef enum {
     CCSprite *pauseMenu;
     CCSprite *problemComplete;
     CCSprite *problemIncomplete;
+    
+    BOOL autoMoveToNextProblem;
+    float moveToNextProblemTime;
 }
 
 @property (retain) Daemon *Zubi;

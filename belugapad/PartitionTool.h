bae68db54dc21159fbdba88f4c6c6be5a90faa46
Dave Amphlett <dave@productivityballoon.com>
2012-03-30 13:35:35 +0100

    add partition tool game scene

---
+++ belugapad/PartitionTool.h
@@ -0,0 +1,24 @@
+//
+//  PartitionTool.h
+//  belugapad
+//
+//  Created by David Amphlett on 29/03/2012.
+//  Copyright (c) 2012 Productivity Balloon Ltd. All rights reserved.
+//
+
+#import "DWGameObject.h"
+#import "ToolConsts.h"
+#import "ToolScene.h"
+
+@interface PartitionTool : ToolScene
+{
+    ToolHost *toolHost;
+    DWGameWorld *gw;
+    NSDictionary *problemDef;
+    
+    CGPoint winL;
+    float cx, cy, lx, ly;
+
+    CCLayer *renderLayer;
+}
+@end
---
+++ belugapad/PartitionTool.m
@@ -0,0 +1,61 @@
+//
+//  PartitionTool.m
+//  belugapad
+//
+//  Created by David Amphlett on 29/03/2012.
+//  Copyright (c) 2012 Productivity Balloon Ltd. All rights reserved.
+//
+
+#import "PartitionTool.h"
+#import "ToolHost.h"
+
+@implementation PartitionTool
+-(id)initWithToolHost:(ToolHost *)host andProblemDef:(NSDictionary *)pdef
+{
+    toolHost=host;
+    problemDef=pdef;
+    
+    if(self=[super init])
+    {
+        //this will force override parent setting
+        //TODO: is multitouch actually required on this tool?
+        [[CCDirector sharedDirector] openGLView].multipleTouchEnabled=YES;
+        
+        CGSize winsize=[[CCDirector sharedDirector] winSize];
+        winL=CGPointMake(winsize.width, winsize.height);
+        lx=winsize.width;
+        ly=winsize.height;
+        cx=lx / 2.0f;
+        cy=ly / 2.0f;
+        
+        gw = [[DWGameWorld alloc] initWithGameScene:self];
+        gw.Blackboard.inProblemSetup = YES;
+        
+        self.BkgLayer=[[[CCLayer alloc]init] autorelease];
+        self.ForeLayer=[[[CCLayer alloc]init] autorelease];
+
+        [toolHost addToolBackLayer:self.BkgLayer];
+        [toolHost addToolForeLayer:self.ForeLayer];
+        
+        
+        [gw Blackboard].hostCX = cx;
+        [gw Blackboard].hostCY = cy;
+        [gw Blackboard].hostLX = lx;
+        [gw Blackboard].hostLY = ly;
+        
+        [gw handleMessage:kDWsetupStuff andPayload:nil withLogLevel:0];
+        
+        gw.Blackboard.inProblemSetup = NO;
+        
+    }
+    
+    return self;
+}
+
+-(void)doUpdateOnTick:(ccTime)delta
+{
+	[gw doUpdate:delta];
+    
+
+}
+@end

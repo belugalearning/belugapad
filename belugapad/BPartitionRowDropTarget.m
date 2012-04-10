3fd041a7edfdc1b636653a03a2511ccb307e2bf4
Dave Amphlett <dave@productivityballoon.com>
2012-04-10 11:29:20 +0100

    re-enable tinting on hover

--- belugapad/BPartitionRowDropTarget.m
+++ belugapad/BPartitionRowDropTarget.m
@@ -57,19 +57,20 @@
                 }
                 DWPartitionObjectGameObject *newO = (DWPartitionObjectGameObject*)gameWorld.Blackboard.PickupObject;
                 if(myHeldValue+newO.Length<=prgo.Length)
-                {                
+                {           
+                    // change the hover-over tint colour
                     for(CCSprite *s in prgo.BaseNode.children)
                     {
-                        [s setColor:ccc3(255,0,0)];
+                        [s setColor:ccc3(0,255,0)];
                     }
                     gameWorld.Blackboard.DropObject=gameObject;
                     //gameWorld.Blackboard.DropObjectDistance=dist;
-                    NSLog(@"hover over row drop target");
                     
                 }
 
             }
         else {
+            // if not a valid droptarget, not in view, set all sprites back to their proper colour
             for(CCSprite *s in prgo.BaseNode.children)
             {
                 [s setColor:ccc3(255,255,255)];

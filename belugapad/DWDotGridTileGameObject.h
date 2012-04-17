9c5bb9c219998eeb06054073521ff4305f6807ba
Dave Amphlett <dave@36peas.com>
2012-04-17 12:16:39 +0100

    add behaviours for tile render

--- belugapad/DWDotGridTileGameObject.h
+++ belugapad/DWDotGridTileGameObject.h
@@ -8,7 +8,22 @@
 
 #import "DWGameObject.h"
 
+typedef enum {
+    kTopLeft=0,
+    kTopRight=1,
+    kBottomLeft=2,
+    kBottomRight=3,
+    kBorderLeft=4,
+    kBorderRight=5,
+    kBorderTop=6,
+    kBorderBottom=7,
+    kNoBorder=8
+} TileType;
+
 @interface DWDotGridTileGameObject : DWGameObject
 
+@property TileType tileType;
+@property (retain) CCSprite *mySprite;
+@property CGPoint Position;
 
 @end
--- belugapad/DWDotGridTileGameObject.m
+++ belugapad/DWDotGridTileGameObject.m
@@ -10,5 +10,8 @@
 
 @implementation DWDotGridTileGameObject
 
+@synthesize tileType;
+@synthesize mySprite;
+@synthesize Position;
 
 @end

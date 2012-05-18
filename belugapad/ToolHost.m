d248a9ab801d4ad78b8b8e73056e76374aef3992
Gareth Jenkins <gareth@36peas.com>
2012-05-18 12:11:00 +0100

    tool host transition timers, tests

--- belugapad/ToolHost.m
+++ belugapad/ToolHost.m
@@ -69,10 +69,7 @@ static float kMoveToNextProblemTime=2.0f;
         perstLayer=[[CCLayer alloc] init];
         [self addChild:perstLayer z:0];
         
-        //add background to background layer
-        CCSprite *bkg=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/bg/bg-ipad.png")];
-        [bkg setPosition:ccp(cx, cy)];
-        [backgroundLayer addChild:bkg z:0];
+        [self animateBackgroundIn];
         
         //add a pause button but keep it hidden -- to be brought in by the fader
         CCSprite *pause=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/menu/button-pause.png")];
@@ -94,7 +91,9 @@ static float kMoveToNextProblemTime=2.0f;
         self.DynProblemParser=[[DProblemParser alloc] init];
         
         contentService = ((AppController*)[[UIApplication sharedApplication] delegate]).contentService;        
-        [self gotoNewProblem];
+        
+        [self scheduleOnce:@selector(gotoFirstProblem:) delay:3.0f];
+        //[self gotoNewProblem];
         
         [self schedule:@selector(doUpdateOnTick:) interval:1.0f/60.0f];
         [self schedule:@selector(doUpdateOnSecond:) interval:1.0f];
@@ -104,6 +103,63 @@ static float kMoveToNextProblemTime=2.0f;
     
     return self;
 }
+
+#pragma mark animation and transisitons -- to factor out to animation providor
+
+-(void) animateBackgroundIn
+{
+    //all -top2 images are loaded at -1.5*cy to place top1 of the top2 in centre frame on start with bg layer at 0,0
+    
+    //add bases to background layer
+    bgBase1=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/ttbg/tx-base-layer-1x-top2.png")];
+    [bgBase1 setPosition:ccp(cx, ly * -0.0f)];
+    [backgroundLayer addChild:bgBase1 z:0];
+    
+    
+    //manually offsetting this
+    bgWater1=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/ttbg/tx-water-1x-top2.png")];
+    [bgWater1 setPosition:ccp(cx, ly * -0.15)];
+    [backgroundLayer addChild:bgWater1 z:0];
+    
+    //needs offset built in as 0.5*ly bigger
+    bgSun1=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/ttbg/tx-sun-1x-top2.5.png")];
+    [bgSun1 setPosition:ccp(cx, ly * -0.25f)];
+    [bgSun1 setOpacity:50];
+    [backgroundLayer addChild:bgSun1 z:0];
+    
+    CCRotateBy *r1=[CCRotateBy actionWithDuration:6.0f angle:7.0f];
+    CCEaseInOut *sunease1=[CCEaseInOut actionWithAction:r1 rate:2.0f];
+    CCRotateBy *r2=[CCRotateBy actionWithDuration:6.0f angle:-7.0f];
+    CCEaseInOut *sunease2=[CCEaseInOut actionWithAction:r2 rate:2.0f];
+    CCSequence *s=[CCSequence actions:sunease1, sunease2, nil];
+    CCRepeatForever *rp=[CCRepeatForever actionWithAction:s];
+    [bgSun1 runAction:rp];
+
+    bgMountain1=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/ttbg/tx-mountains-1x.png")];
+    [bgMountain1 setPosition:ccp(lx, cy)];
+    [backgroundLayer addChild:bgMountain1 z:0];
+    
+    
+    [self scheduleOnce:@selector(moveToTool1:) delay:1.5f];
+}
+     
+-(void) moveToTool1: (ccTime) delta
+{
+    CCMoveBy *mv=[CCMoveBy actionWithDuration:1.5f position:ccp(0, ly)];
+    CCEaseInOut *ease=[CCEaseInOut actionWithAction:mv rate:2.0f];
+    [backgroundLayer runAction:ease];
+    
+    //move the sun quicker still
+    CCMoveBy *mvSun=[CCMoveBy actionWithDuration:1.5f position:ccp(0, 0.25*ly)];
+    [bgSun1 runAction:mvSun];
+    
+    //move the mountain
+    CCMoveBy *mvMountain=[CCMoveBy actionWithDuration:1.5f position:ccp(cx, 0)];
+    [bgMountain1 runAction:mvMountain];
+    
+}
+
+#pragma mark
 
 -(void)draw
 {
@@ -217,6 +273,11 @@ static float kMoveToNextProblemTime=2.0f;
         [[CCDirector sharedDirector] view].multipleTouchEnabled=NO;
 
     
+}
+
+-(void) gotoFirstProblem: (ccTime) delta
+{
+    [self gotoNewProblem];
 }
 
 -(void) gotoNewProblem

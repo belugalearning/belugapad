2c91d8eb24174d6de29ec211ff48b35cb42e7b7e
Gareth Jenkins <gareth@36peas.com>
2012-05-25 12:15:35 +0100

    NordicAnimator

---
+++ belugapad/NordicAnimator.h
@@ -0,0 +1,13 @@
+//
+//  NordicAnimator.h
+//  belugapad
+//
+//  Created by Gareth Jenkins on 25/05/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NordicAnimator : NSObject
+
+@end
---
+++ belugapad/NordicAnimator.m
@@ -0,0 +1,13 @@
+//
+//  NordicAnimator.m
+//  belugapad
+//
+//  Created by Gareth Jenkins on 25/05/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NordicAnimator.h"
+
+@implementation NordicAnimator
+
+@end

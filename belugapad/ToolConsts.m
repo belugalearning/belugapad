0e9befbfe174d5e893f735a3725ff29bea1f2a1c
Dave Amphlett <dave@36peas.com>
2012-05-28 14:50:12 +0100

    fixes #29

--- belugapad/ToolConsts.m
+++ belugapad/ToolConsts.m
@@ -18,7 +18,8 @@ const float kButtonNextToolHitXOffset=975.0f;
 
 
 const float kLabelSubTitleYOffsetHalfProp=1.75f;
-const float kLabelTitleYOffsetHalfProp=1.85f;
+const float kLabelTitleXMarginProp=0.9f;
+const float kLabelTitleYOffsetHalfProp=1.47f;
 const ccColor3B kLabelTitleColor={255, 255, 255};
 
 const float kLabelCompleteYOffsetHalfProp=0.28f;

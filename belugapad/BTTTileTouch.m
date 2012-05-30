3dd4001d957eb5e7cd7c7dd624e569daef42eb48
Dave Amphlett <dave@productivityballoon.com>
2012-05-30 20:30:34 +0100

    change text colour for user-revealed answers (timetables)

--- belugapad/BTTTileTouch.m
+++ belugapad/BTTTileTouch.m
@@ -121,7 +121,7 @@
                 else if(tile.operatorType==kOperatorDiv)tile.myText=[CCLabelTTF labelWithString:[NSString stringWithFormat:@"%g", tile.myXpos/tile.myYpos] fontName:PROBLEM_DESC_FONT fontSize:PROBLEM_DESC_FONT_SIZE]; 
                 
                 [tile.myText setPosition:[tile.mySprite convertToNodeSpace:tile.Position]];
-                [tile.myText setColor:ccc3(83,93,100)];
+                [tile.myText setColor:ccc3(200,200,200)];
                 [tile.mySprite addChild:tile.myText];
 
             }

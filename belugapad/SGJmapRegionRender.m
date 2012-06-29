5dd3b087e0b372ec856d0049acf365437e77d8e5
Gareth Jenkins <gareth@36peas.com>
2012-06-30 00:05:28 +0100

    region ready render -- mastery parse for outer point definition & sort

--- belugapad/SGJmapRegionRender.m
+++ belugapad/SGJmapRegionRender.m
@@ -8,6 +8,8 @@
 
 #import "SGJmapRegionRender.h"
 #import "SGJmapRegion.h"
+#import "SGJmapMasteryNode.h"
+#import "BLMath.h"
 
 @implementation SGJmapRegionRender
 
@@ -36,12 +38,77 @@
 
 -(void)readyRender
 {
-    NSLog(@"ready to render with %d mastery children", ParentGO.MasteryNodes.count);
+    NSLog(@"ready to render region with %d mastery children", ParentGO.MasteryNodes.count);
+    
+    //===== get my position =============================================================
+    float cumx=0.0f, cumy=0.0f;
+    for(SGJmapMasteryNode *mnode in ParentGO.MasteryNodes)
+    {
+        cumx+=mnode.Position.x;
+        cumy+=mnode.Position.y;
+    }
+    
+    ParentGO.Position=CGPointMake(cumx / (float)ParentGO.MasteryNodes.count, cumy / (float)ParentGO.MasteryNodes.count);
+    
+    
+    //====== create list of vertices, sorted from mastery nodes =========================
+    NSMutableArray *verts=[[NSMutableArray alloc] init];
+    
+    //collect points from childen and sort on the way
+    for(SGJmapMasteryNode *mnode in ParentGO.MasteryNodes)
+    {
+        if(verts.count==0)
+        {
+            [verts addObject:[NSValue valueWithCGPoint:mnode.Position]];
+        }
+        else {
+            float thisA=[BLMath angleFromNorthToLineFrom:ParentGO.Position to:mnode.Position];
+            for(int i=0; i<verts.count; i++)
+            {
+                int insertat=0;
+                BOOL doinsert=NO;
+                
+                CGPoint snodePos=[[verts objectAtIndex:i] CGPointValue];
+                float nextA=[BLMath angleFromNorthToLineFrom:ParentGO.Position to:snodePos];
+                
+                if(nextA>thisA)
+                {
+                    //angle of next item in verts is greater, so insert before
+                    doinsert=YES;
+                    insertat=i;
+                }
+                else if(i==verts.count-1)
+                {
+                    //no more angles to compare to, so default insert last
+                    doinsert=YES;
+                    insertat=verts.count;
+                }
+                
+                if(doinsert)
+                {
+                    [verts insertObject:[NSValue valueWithCGPoint:snodePos] atIndex:insertat];
+                    break;
+                }
+            }
+        }
+    }
+    
+    //====== insert spacers as required ===============================================
+    
+    
+    //====== malloc array for points themselves =======================================
+    
+    
+    
+    //====== iterate verts, scale up & out and add to the malloc'd array to render ====
+    
+    
+    
+
 }
 
 -(void)draw:(int)z
 {
-    
 }
 
 -(void)dealloc

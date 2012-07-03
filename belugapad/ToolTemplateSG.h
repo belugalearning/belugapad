319d82eb987e287e4cc97de6d4e2be6c8d341d5e
Dave Amphlett <dave@36peas.com>
2012-07-03 11:11:08 +0100

    add tool template file for SG Gameworld

---
+++ belugapad/ToolTemplateSG.h
@@ -0,0 +1,57 @@
+//
+//  ToolTemplateSG.h
+//  belugapad
+//
+//  Created by Gareth Jenkins on 23/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "cocos2d.h"
+#import "ToolConsts.h"
+#import "ToolScene.h"
+
+@interface ToolTemplateSG : CCLayer
+{
+    // required toolhost stuff
+    ToolHost *toolHost;
+    
+    // standard Problem Definition stuff
+    ProblemEvalMode evalMode;
+    ProblemRejectMode rejectMode;
+    ProbjemRejectType rejectType;
+    
+    // default positional bits
+    CGPoint winL;
+    float cx, cy, lx, ly;
+    
+    // common touch interactions
+    BOOL isTouching;
+    CGPoint lastTouch;
+    
+    // standard to move between problems
+    float timeToAutoMoveToNextProblem;
+    BOOL autoMoveToNextProblem;
+    
+    // and a default layer
+    CCLayer *renderLayer;
+}
+
++(CCScene *)scene;
+-(id)init;
+-(void)populateGW;
+-(void)readPlist;
+-(void)doUpdate:(ccTime)delta;
+-(void)doUpdateProximity:(ccTime)delta;
+-(void)draw;
+-(BOOL)evalExpression;
+-(void)evalProblem;
+-(void)resetProblem;
+-(float)metaQuestionTitleYLocation;
+-(float)metaQuestionAnswersYLocation;
+-(void)ccTouchesBegan:(NSSet *)touches withEvent:(UIEvent *)event;
+-(void)ccTouchesMoved:(NSSet *)touches withEvent:(UIEvent *)event;
+-(void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event;
+-(void)ccTouchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event;
+-(void)dealloc;
+
+@end

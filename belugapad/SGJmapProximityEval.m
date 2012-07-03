ba141b3b8706201aaf57484faf463f600ca6cf37
Gareth Jenkins <gareth@36peas.com>
2012-07-03 17:55:52 +0100

    tidying message sends to id in proximity eval

--- belugapad/SGJmapProximityEval.m
+++ belugapad/SGJmapProximityEval.m
@@ -30,14 +30,14 @@ static float visibleProximity=1024.0f;
         if(!ParentGO.Visible)
         {
             ParentGO.Visible=YES;
-            [ParentGO handleMessage:kSGvisibilityChanged andPayload:nil withLogLevel:0];
+            [ParentGO handleMessage:kSGvisibilityChanged];
         }
     }
     else {
         if(ParentGO.Visible)
         {
             ParentGO.Visible=NO;
-            [ParentGO handleMessage:kSGvisibilityChanged andPayload:nil withLogLevel:0];
+            [ParentGO handleMessage:kSGvisibilityChanged];
         }
     }
 }

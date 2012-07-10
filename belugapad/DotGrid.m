949e23246227e882f3f6bd40be1e884d90bbf45b
Dave Amphlett <dave@36peas.com>
2012-07-10 14:22:16 +0100

    add a default anchor_space if not specified of 85

--- belugapad/DotGrid.m
+++ belugapad/DotGrid.m
@@ -122,7 +122,12 @@
     evalDividend=[[pdef objectForKey:DOTGRID_EVAL_DIVIDEND] intValue];
     evalDivisor=[[pdef objectForKey:DOTGRID_EVAL_DIVISOR] intValue];
     evalTotalSize=[[pdef objectForKey:DOTGRID_EVAL_TOTALSIZE] intValue];
-    spaceBetweenAnchors=[[pdef objectForKey:ANCHOR_SPACE] intValue];
+    
+    if([pdef objectForKey:ANCHOR_SPACE])
+        spaceBetweenAnchors=[[pdef objectForKey:ANCHOR_SPACE] intValue];
+    else 
+        spaceBetweenAnchors=85;
+    
     startX=[[pdef objectForKey:START_X] intValue];
     startY=[[pdef objectForKey:START_Y] intValue];
     if([pdef objectForKey:INIT_OBJECTS])initObjects=[pdef objectForKey:INIT_OBJECTS];

c7b00426ec58a8ffe56139f7b145b95f9c66f9ea
Gareth Jenkins <gareth@36peas.com>
2012-07-22 23:49:15 +0100

    new actions: highlightincrease, fallreturn, fadeoutin, fadeoutinto, scaleoutreturn

--- belugapad/InteractionFeedback.m
+++ belugapad/InteractionFeedback.m
@@ -51,6 +51,44 @@
     return oe;
 }
 
++(CCAction*)highlightIncreaseAction
+{
+    //short cut this to drop and bouce for now
+    return [self dropAndBounceAction];
+}
+
++(CCAction*)fallAndReturn
+{
+    CCMoveBy *mv=[CCEaseInOut actionWithAction:[CCMoveBy actionWithDuration:0.5f position:ccp(0, -100)] rate:2.0f];
+    CCMoveBy *mup=[CCEaseInOut actionWithAction:[CCMoveBy actionWithDuration:0.1f position:ccp(0, 100)] rate:2.0f];
+    CCSequence *smove=[CCSequence actions:mv, mup, nil];
+    return smove;
+}
+
++(CCAction*)scaleOutReturn
+{
+    CCMoveBy *mv=[CCEaseInOut actionWithAction:[CCScaleTo actionWithDuration:0.5f scale:0.0f] rate:2.0f];
+    CCMoveBy *mup=[CCEaseInOut actionWithAction:[CCScaleTo actionWithDuration:0.1f scale:1.0f] rate:2.0f];
+    CCSequence *smove=[CCSequence actions:mv, mup, nil];
+    return smove;
+}
+
++(CCAction*)fadeOutIn;
+{
+    CCFadeOut *fo=[CCFadeOut actionWithDuration:0.5f];
+    CCFadeIn *fi=[CCFadeIn actionWithDuration:0.1f];
+    CCSequence *sfade=[CCSequence actions:fo, fi, nil];
+    return sfade;
+}
+
++(CCAction*)fadeOutInTo:(GLubyte)opacity
+{
+    CCFadeTo *fo=[CCFadeTo actionWithDuration:0.5f opacity:0];
+    CCFadeTo *fi=[CCFadeTo actionWithDuration:0.1f opacity:opacity];
+    CCSequence *sfade=[CCSequence actions:fo, fi, nil];
+    return sfade;    
+}
+
 +(CCAction*)enlargeTo1xAction
 {
     //todo: consider expanding to a "pickup" action, would need texture swap out params and fixed scaling

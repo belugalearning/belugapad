8d542261ff0afb30219031f7ba6a2a16e19ae34f
Dave Amphlett <dave@36peas.com>
2012-09-04 13:59:58 +0100

    remove duplicate addition of render layer to fore layer in pie splitter

---
+++ belugapad/SGFBlockOpBubble.h
@@ -0,0 +1,13 @@
+//
+//  SGFBlockOpBubble.h
+//  belugapad
+//
+//  Created by David Amphlett on 04/09/2012.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SGFBlockOpBubble : NSObject
+
+@end

0837b26eb5e86d432840ab850779c2945b3a913b
Gareth Jenkins <gareth@36peas.com>
2012-09-24 16:30:10 +0100

    btxe b:on parsing samples

---
+++ belugapad/SGBtxeObjectNumber.h
@@ -0,0 +1,13 @@
+//
+//  SGBtxeObjectNumber.h
+//  belugapad
+//
+//  Created by gareth on 24/09/2012.
+//
+//
+
+#import "SGGameObject.h"
+
+@interface SGBtxeObjectNumber : SGGameObject
+
+@end
---
+++ belugapad/SGBtxeObjectNumber.m
@@ -0,0 +1,13 @@
+//
+//  SGBtxeObjectNumber.m
+//  belugapad
+//
+//  Created by gareth on 24/09/2012.
+//
+//
+
+#import "SGBtxeObjectNumber.h"
+
+@implementation SGBtxeObjectNumber
+
+@end

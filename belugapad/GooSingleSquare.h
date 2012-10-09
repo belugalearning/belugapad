6704d1ffed31e3f5b3d44c5e713b51be16ef2a2a
Gareth Jenkins <gareth@36peas.com>
2012-10-09 13:34:22 +0100

    goo sample problems

---
+++ belugapad/GooSingleSquare.h
@@ -0,0 +1,13 @@
+//
+//  GooSingleSquare.h
+//  belugapad
+//
+//  Created by gareth on 09/10/2012.
+//
+//
+
+#import "GooSingle.h"
+
+@interface GooSingleSquare : GooSingle
+
+@end
---
+++ belugapad/GooSingleSquare.m
@@ -0,0 +1,13 @@
+//
+//  GooSingleSquare.m
+//  belugapad
+//
+//  Created by gareth on 09/10/2012.
+//
+//
+
+#import "GooSingleSquare.h"
+
+@implementation GooSingleSquare
+
+@end

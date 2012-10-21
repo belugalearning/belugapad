219ac7d408c6602074792cfc55d8b2c19c30f060
Nicholas Cartwright <nc@belugalearning.com>
2012-10-21 06:00:25 +0100

    maintain undo/redo stack for edits

--- edit-pdef/edit-pdef-client-files/pdef-edit.js
+++ edit-pdef/edit-pdef-client-files/pdef-edit.js
@@ -1,8 +1,8 @@
 var ios = navigator.userAgent.match(/iphone|ipad|ipod/i) !== null
   , pdef
   , changeStack = []
-  , currStackIndex
-  , lastSaveStackIndex
+  , currStackIndex = 0
+  , lastSaveStackIndex = 0
 
 $(function() {
   setEnableExpandCollapse(true)
@@ -25,9 +25,7 @@ $(function() {
       $('div[data-type].selected').removeClass('selected')
       $(e.currentTarget).addClass('selected')
     })
-    .on('click', 'div[data-type].selected > span > input[type="button"][value="del"]', function() {
-      $(this).closest('div[data-type]').remove()
-    })
+    .on('click', 'div[data-type].selected > span > input[type="button"][value="del"]', deleteKey)
 
   // test-edits button listener
   $('input[type="button"][value="test"]').on('click', function() {
@@ -47,7 +45,7 @@ var appInterface = {
   loadPDef: function(pDef) {
     pdef = pDef
 
-    // wihout delay some images don't load
+    // TODO: investigate - wihout delay some images don't load, arbitrary time delay hardly reliable
     setTimeout(function() {
       jade.render($('#pdef-wrapper')[0], 'parse-pdef-template', { key:'root', value:pdef, level:0 })
     }, 100)
@@ -81,20 +79,47 @@ function setEnableExpandCollapse(on) {
     var $p = $(this).parent()
     $p.hasClass('collapsed') ? $p.removeClass('collapsed') : $p.addClass('collapsed')
   }
-
   $(document)[on ? 'on' : 'off']('click', 'div[data-type~="collection"] > span.expand-collapse', fn)
+}
+
+function deleteKey(e) {
+  var path = pathToElement(e.target)
+  recordChange({
+    type:'delete-key'
+    , key: path[0]
+    , parentPath:path.splice(1)
+  }) && $(this).closest('div[data-type]').remove()
 }
 
 function setEnableEditKey(on) {
   var fn = arguments.callee.fn = arguments.callee.fn || function() {
     var $span = $(this)
-    , $input = $('<input type="text"/>').val($span.text())
+    , oldKey = $span.text()
+    , $input = $('<input type="text"/>').val(oldKey)
 
     $span.html($input)
     $input.focus()
 
     $input.on('focusout', function() {
-      $span.html($input.val())
+      var newKey = $input.val().trim()
+      if (!newKey || newKey === oldKey) {
+        $span.html(oldKey)
+      } else if ($span.parent().siblings('[data-key="' + newKey + '"]').length) {
+        $span.html(oldKey)
+        alert('error: duplicate key')
+      } else {
+        var path = pathToElement($span)
+        $span
+          .html(newKey)
+          .closest('[data-key]').attr('data-key', newKey)
+
+        recordChange({
+          type: 'edit-key'
+          , parentPath: path.slice(1)
+          , oldKey: oldKey
+          , newKey: newKey
+        })
+      }
     })
     return false
   }
@@ -107,17 +132,32 @@ function setEnableEditValue(on) {
     var $span = $(this)
       , oldVal = $span.text()
       , w = $('#pdef-wrapper').width() - $span.offset().left
-      , $input = $('<input type="text"/>').val(oldVal).width(w)
+      , $dt = $span.closest('[data-type]')
+      , isNumber = $dt.is('[data-type~="Number"]')
+      , inputType = isNumber ? 'number' : 'text'
+      , $input = $('<input type="'+inputType+'"/>').val(oldVal).width(w)
 
     $span.html($input)
     $input.focus()
 
-    $input.on('focusout', function() {
+    $input.on('change', function() {
       var newVal = $input.val()
-      $span.html(newVal)
-      if (newVal !== oldVal) {
-        //onChangeValue($span)
+
+      if (newVal === '') {
+        $span.html(oldVal)
+        return
       }
+
+      if (isNumber) newVal = Number(newVal)
+      var path = pathToElement($span)
+      $span.html(newVal)
+      recordChange({
+        type: 'edit-value'
+        , parentPath: path.splice(1)
+        , key: path[0]
+        , oldVal: oldVal
+        , newVal: newVal
+      })
     })
     return false
   }
@@ -160,4 +200,80 @@ function getJSON() {
       return $el.children('[data-field~="value"]').text()
     }
   })($('[data-type]:first'))
+}
+
+function pathToElement(el) {
+  var stack = $(el).parents('[data-type][data-key!="root"]').toArray()
+  if ($(el).is('[data-type]')) stack.unshift(el)
+
+  return stack
+    .map(function(dt, i) {
+      return $(stack[i+1]).is('[data-type~="Array"]')
+        ? $(dt).siblings('[data-type]').andSelf().index(dt)
+        : $(dt).attr('data-key')
+    })
+}
+
+function elementSelectorFromPath(path) {
+  var sel = '#pdef-wrapper > [data-type][data-key="root"]'
+  for (var i=path.length, part; part=path[--i];) {
+    if (typeof part == 'number') {
+      sel += ' > [data-type]:eq(' + part + ')'
+    } else {
+      sel += ' > [data-type][data-key="' + part + '"]'
+    }
+  }
+  return sel
+}
+
+function objAtPath(path) {
+  var o = pdef
+    , i = path.length
+  while (i-- && o) o = o[path[i]]
+  return o
+}
+
+function recordChange(change) {
+  switch(change.type) {
+    case 'edit-key':
+      var parent = objAtPath(change.parentPath)
+      if (!parent) {
+        var path = Object.prototype.toString.call(change.parentPath) == '[object Array]' ? change.parentPath.join(' < ') : '[BAD PATH]'
+        alert('parent not found at path:' + path)
+        return false
+      }
+      parent[change.newKey] = parent[change.oldKey]
+      delete parent[change.oldKey]
+      break
+    case 'delete-key':
+      var parent = objAtPath(change.parentPath)
+      if (!parent) {
+        var path = Object.prototype.toString.call(change.parentPath) == '[object Array]' ? change.parentPath.join(' < ') : '[BAD PATH]'
+        alert('parent not found at path:' + path)
+        return false
+      }
+      change.value = parent[change.key]
+      delete parent[change.key]
+      break
+    case 'edit-value':
+      var parent = objAtPath(change.parentPath)
+      if (!parent) {
+        var pathString = Object.prototype.toString.call(change.parentPath) == '[object Array]' ? change.parentPath.join(' < ') : '[BAD PATH]'
+        alert('parent not found at path:' + pathString)
+        return false
+      }
+      if (typeof parent[change.key] == 'undefined') {
+        alert('key not found at path:' + change.key + ' < ' + change.parentPath.join(' < '))
+        return false
+      }
+      parent[change.key] = change.newVal
+      break
+    default:
+      alert('unhandled change type: ' + change.type)
+      return false
+  }
+  if (lastSaveStackIndex > currStackIndex++) lastSaveStackIndex = null
+  changeStack.splice(currStackIndex)
+  changeStack.push(change)
+  return true
 }

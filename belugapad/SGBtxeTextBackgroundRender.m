e83c37ddd832dd32e06a978e4feb8d561b3330aa
Dave Amphlett <dave@36peas.com>
2012-11-21 01:13:29 +0000

    update BTXE placeholder assets

--- belugapad/SGBtxeTextBackgroundRender.m
+++ belugapad/SGBtxeTextBackgroundRender.m
@@ -7,6 +7,7 @@
 //
 
 #import "SGBtxeTextBackgroundRender.h"
+#import "SGBtxePlaceholder.h"
 #import "global.h"
 
 @implementation SGBtxeTextBackgroundRender
@@ -33,10 +34,29 @@
 
 -(void)setupDrawWithSize:(CGSize)size
 {
+    BOOL isPlaceholder=NO;
     backgroundNode=[[CCNode alloc]init];
-    CCSprite *lh=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/btxe/SB_Block_Question_Left.png")];
-    CCSprite *m=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/btxe/SB_Block_Question_Middle.png")];
-    CCSprite *rh=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/btxe/SB_Block_Question_Right.png")];
+    CCSprite *lh=nil;
+    CCSprite *m=nil;
+    CCSprite *rh=nil;
+    
+    if([(id<NSObject>)ParentGO isKindOfClass:[SGBtxePlaceholder class]])
+        isPlaceholder=YES;
+    
+    if(isPlaceholder)
+    {
+        lh=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/btxe/SB_Holder_Large_Left.png")];
+        m=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/btxe/SB_Holder_Large_Middle.png")];
+        rh=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/btxe/SB_Holder_Large_Right.png")];
+//        [rh setPosition:ccp(((m.contentSize.width)*m.scaleX)+(rh.contentSize.width)*2, 0)];
+    }
+    else
+    {
+        lh=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/btxe/SB_Block_Question_Left.png")];
+        m=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/btxe/SB_Block_Question_Middle.png")];
+        rh=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/btxe/SB_Block_Question_Right.png")];
+//        [rh setPosition:ccp(m.contentSize.width/2+(rh.contentSize.width/2), 0)];
+    }
     
     m.scaleX=size.width / m.contentSize.width;
     m.scaleY=size.height / m.contentSize.height;
@@ -47,9 +67,28 @@
     [self.backgroundNode setPosition:ccp(0, -3)];
     [lh setPosition:ccp(-((m.contentSize.width/2)*m.scaleX)-(lh.contentSize.width/2),0)];
     [rh setPosition:ccp(((m.contentSize.width/2)*m.scaleX)+(rh.contentSize.width/2), 0)];
+
     [backgroundNode addChild:lh];
     [backgroundNode addChild:m];
     [backgroundNode addChild:rh];
+    
+//    backgroundNode=[[CCNode alloc]init];
+//    CCSprite *lh=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/btxe/SB_Block_Question_Left.png")];
+//    CCSprite *m=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/btxe/SB_Block_Question_Middle.png")];
+//    CCSprite *rh=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/btxe/SB_Block_Question_Right.png")];
+//    
+//    m.scaleX=size.width / m.contentSize.width;
+//    m.scaleY=size.height / m.contentSize.height;
+//    
+//    lh.scaleY=size.height / m.contentSize.height;
+//    rh.scaleY=size.height / m.contentSize.height;
+//    
+//    [self.backgroundNode setPosition:ccp(0, -3)];
+//    [lh setPosition:ccp(-((m.contentSize.width/2)*m.scaleX)-(lh.contentSize.width/2),0)];
+//    [rh setPosition:ccp(((m.contentSize.width/2)*m.scaleX)+(rh.contentSize.width/2), 0)];
+//    [backgroundNode addChild:lh];
+//    [backgroundNode addChild:m];
+//    [backgroundNode addChild:rh];
 }
 
 -(void)updatePosition:(CGPoint)position

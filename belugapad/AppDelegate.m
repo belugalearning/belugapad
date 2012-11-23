c3ee9307cd60cd08c7b603c964fa963f4c3ebea8
Nicholas Cartwright <nc@belugalearning.com>
2012-11-23 14:14:22 +0000

    s/USE_TESTGLIHT_SDK/USE_TESTFLIGHT_SDK

--- belugapad/AppDelegate.m
+++ belugapad/AppDelegate.m
@@ -68,7 +68,7 @@
     
     //init test flight
 
-#if USE_TESTGLIHT_SDK
+#if USE_TESTFLIGHT_SDK
     [TestFlight setDeviceIdentifier:[[UIDevice currentDevice] uniqueIdentifier]];
 
     [TestFlight setOptions:[NSDictionary dictionaryWithObject:[NSNumber numberWithBool:NO] forKey:@"logToSTDERR"]];

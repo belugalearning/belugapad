0357aca7a219c178f22b4233d4279fe6417e22b0
Gareth Jenkins <gareth@36peas.com>
2012-11-23 19:18:46 +0000

    only make large rows for sequencing evaluations

---
+++ belugapad/SGBtxeNumberDotRender.h
@@ -0,0 +1,13 @@
+//
+//  SGBtxeNumberDotRender.h
+//  belugapad
+//
+//  Created by gareth on 23/11/2012.
+//
+//
+
+#import "SGComponent.h"
+
+@interface SGBtxeNumberDotRender : SGComponent
+
+@end
---
+++ belugapad/SGBtxeNumberDotRender.m
@@ -0,0 +1,13 @@
+//
+//  SGBtxeNumberDotRender.m
+//  belugapad
+//
+//  Created by gareth on 23/11/2012.
+//
+//
+
+#import "SGBtxeNumberDotRender.h"
+
+@implementation SGBtxeNumberDotRender
+
+@end

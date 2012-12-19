d72b403d5ceb7a0faf1f43fd5a82afc5a55c88ae
Dave Amphlett <dave@36peas.com>
2012-12-19 11:14:13 +0000

    labelly stuff for sunken islands

--- belugapad/SGJmapComingSoonNode.m
+++ belugapad/SGJmapComingSoonNode.m
@@ -52,6 +52,28 @@
     CCSprite *nodeSprite=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(spriteFileName)];
     [nodeSprite setPosition:self.Position];
     [nodeSprite setVisible:YES];
+    
+    NSString *labelText=UserVisibleString;
+    
+    CGPoint labelCentre=ccpAdd(ccp(75, -125), Position);
+    CCLabelTTF *labelShadowSprite=[CCLabelTTF labelWithString:labelText fontName:@"Chango" fontSize:20.0f];
+    [labelShadowSprite setPosition:ccpAdd(labelCentre, ccp(0, -3))];
+    //[labelShadowSprite setRotation:[[islandData objectForKey:ISLAND_LABEL_ROT] floatValue]];
+    [labelShadowSprite setColor:ccc3(0, 0, 0)];
+    //    [labelShadowSprite setOpacity:0.2f*255];
+    [labelShadowSprite setRotation:-8.0f];
+    [labelShadowSprite setVisible:YES];
+    
+    [RenderLayer addChild:labelShadowSprite z:3];
+    CCLabelTTF *labelSprite=[CCLabelTTF labelWithString:labelText fontName:@"Chango" fontSize:20.0f];
+    [labelSprite setPosition:labelCentre];
+    //[labelSprite setRotation:[[islandData objectForKey:ISLAND_LABEL_ROT] floatValue]];
+    [labelSprite setVisible:YES];
+    //    [labelSprite setOpacity:0.7f*255];
+    [labelSprite setRotation:-8.0f];
+    [RenderLayer addChild:labelSprite z:3];
+    
+    
     [RenderLayer addChild:nodeSprite];
     
 }

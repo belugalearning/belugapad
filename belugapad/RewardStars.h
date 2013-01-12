5a38ed06b7a6dc2ce5bf5499add29a211a3637d1
Dave Amphlett <dave@productivityballoon.com>
2013-01-12 18:42:39 +0000

    animate in of stars

--- belugapad/RewardStars.h
+++ belugapad/RewardStars.h
@@ -15,6 +15,21 @@
     CCLayer *starLayer;
     CCSprite *returnToMap;
     CCSprite *replayNode;
+    UsersService *usersService;
+    
+    float timeSinceStar1;
+    float timeSinceStar2;
+    
+    BOOL fireStar1;
+    BOOL countStar2;
+    BOOL countStar3;
+    
+    int stars;
+    
+    CCSprite *s1;
+    CCSprite *s2;
+    CCSprite *s3;
+    
 }
 
 +(CCScene *)scene;
--- belugapad/RewardStars.m
+++ belugapad/RewardStars.m
@@ -15,6 +15,7 @@
 #import "BLMath.h"
 #import "JMap.h"
 #import "AppDelegate.h"
+#import "UsersService.h"
 #import "ToolHost.h"
 #import "SimpleAudioEngine.h"
 
@@ -31,6 +32,8 @@
     return scene;
 }
 
+
+
 -(id) init
 {
     if(self=[super init])
@@ -43,6 +46,10 @@
         ly=winsize.height;
         cx = lx / 2.0f;
         cy = ly / 2.0f;
+        [self schedule:@selector(doUpdate:) interval:1.0f / 60.0f];
+        
+        AppController *ac = (AppController*)[[UIApplication sharedApplication] delegate];
+        usersService = ac.usersService;
         
         [self setupStars];
     }
@@ -50,18 +57,58 @@
     return self;
 }
 
+-(void)doUpdate:(ccTime)delta
+{
+    if(fireStar1)
+    {
+        [s1 setVisible:YES];
+        [s1 runAction:[self scaleTo1x]];
+        [s1 runAction:[self rotateTo0]];
+        [s1 runAction:[self moveTo:ccp(376,411)]];
+        
+        if(stars>1)
+            countStar2=YES;
+        fireStar1=NO;
+    }
+    
+    if(countStar2)
+        timeSinceStar1+=delta;
+    
+    if(countStar2 && timeSinceStar1>0.5f)
+    {
+        [s2 setVisible:YES];
+        [s2 runAction:[self scaleTo1x]];
+        [s2 runAction:[self rotateTo0]];
+        [s2 runAction:[self moveTo:ccp(510,446)]];
+        countStar3=YES;
+        countStar2=NO;
+    }
+    
+    if(countStar3)
+        timeSinceStar2+=delta;
+    
+    if(countStar3 && timeSinceStar2>0.5f)
+    {
+        [s3 setVisible:YES];
+        [s3 runAction:[self scaleTo1x]];
+        [s3 runAction:[self rotateTo0]];
+        [s3 runAction:[self moveTo:ccp(510,446)]];
+        countStar3=NO;
+    }
+}
+
 #pragma mark - setup and parse
 
 -(void) setupStars;
 {
-    int stars=3;
+    stars=usersService.lastStarAcheived;
     CCSprite *starBkg=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/rewards/final_score_bg.png")];
     [starBkg setPosition:ccp(cx,cy)];
     [self addChild:starBkg];
     
-    CCSprite *s1=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/rewards/final_score_star_1.png")];
-    CCSprite *s2=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/rewards/final_score_star_2.png")];
-    CCSprite *s3=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/rewards/final_score_star_3.png")];
+    s1=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/rewards/final_score_star_1.png")];
+    s2=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/rewards/final_score_star_2.png")];
+    s3=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/rewards/final_score_star_3.png")];
     
     replayNode=[CCSprite spriteWithFile:BUNDLE_FULL_PATH(@"/images/rewards/final_score_replay.png")];
     [replayNode setPosition:ccp(762,227)];
@@ -71,26 +118,52 @@
     [returnToMap setPosition:ccp(351,227)];
     [self addChild:returnToMap];
     
-    [s1 setPosition:ccp(376,411)];
-    [s2 setPosition:ccp(510,443)];
-    [s3 setPosition:ccp(643,411)];
+
     
-    if(stars==1)
-    {
-        [self addChild:s1];
-    }
-    else if(stars==2)
-    {
-        [self addChild:s1];
-        [self addChild:s2];
-    }
-    else if(stars==3)
-    {
-        [self addChild:s1];
-        [self addChild:s2];
-        [self addChild:s3];
-    }
+    [s1 setScale:4.0f];
+    [s2 setScale:4.0f];
+    [s3 setScale:4.0f];
     
+    [s1 setRotation:-90.0f];
+    [s2 setRotation:-90.0f];
+    [s3 setRotation:-90.0f];
+    
+    [s1 setPosition:ccp(0,ly)];
+    [s2 setPosition:ccp(0,ly)];
+    [s3 setPosition:ccp(0,ly)];
+    
+    [s1 setVisible:NO];
+    [s2 setVisible:NO];
+    [s3 setVisible:NO];
+    
+    [self addChild:s1];
+    [self addChild:s2];
+    [self addChild:s3];
+
+    fireStar1=YES;
+    
+//    [s1 runAction:[self scaleTo1xRotateAndPlace:ccp(376,411)]];
+//    [s2 runAction:[self scaleTo1xRotateAndPlace:ccp(510,446)]];
+//    [s3 runAction:[self scaleTo1xRotateAndPlace:ccp(643,411)]];
+    
+}
+
+-(CCScaleTo*)scaleTo1x
+{
+    CCScaleTo *scale=[CCScaleTo actionWithDuration:0.5f scale:1.0f];
+    return scale;
+}
+
+-(CCRotateTo*)rotateTo0
+{
+    CCRotateTo *rotate=[CCRotateTo actionWithDuration:0.5f angle:0.0f];
+    return rotate;
+}
+
+-(CCMoveTo*)moveTo:(CGPoint)here
+{
+    CCMoveTo *move=[CCMoveTo actionWithDuration:0.5f position:here];
+    return move;
 }
 
 #pragma mark - touches

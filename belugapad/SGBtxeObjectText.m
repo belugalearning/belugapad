245bf11152b601091e5bb78eea6de4b1358a90cf
Dave Amphlett <dave@36peas.com>
2013-01-14 17:56:52 +0000

    fix #748. remove lots of redundant code in TH, and comment more that should be redundant.

--- belugapad/SGBtxeObjectText.m
+++ belugapad/SGBtxeObjectText.m
@@ -181,10 +181,7 @@
 
 -(void)tagMyChildrenForIntro
 {
-    [textRenderComponent.label setTag:3];
-    [textRenderComponent.label0 setTag:3];
-    [textRenderComponent.label setOpacity:0];
-    [textRenderComponent.label0 setOpacity:0];
+    [textRenderComponent tagMyChildrenForIntro];
     [textBackgroundRenderComponent tagMyChildrenForIntro];
 }
 

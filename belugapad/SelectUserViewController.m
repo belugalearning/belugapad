1202a3dfde183bee10dc7f1b2c96bf0011294a39
Gareth Jenkins <gareth@36peas.com>
2013-01-16 19:35:50 +0000

    change play>loading on press & load

--- belugapad/SelectUserViewController.m
+++ belugapad/SelectUserViewController.m
@@ -534,6 +534,7 @@
 {
     if (freezeUI) return;
     
+    [loginButton setImage:[UIImage imageNamed:@"/login-images/loading_button.png"] forState:UIControlStateNormal];
     
     if (!selectUserPassCodeModalView.isValid)
     {

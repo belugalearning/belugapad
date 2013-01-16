0298161a5befcaa3f15db8891fa52d9a2cfed733
Gareth Jenkins <gareth@36peas.com>
2013-01-16 14:25:15 +0000

    test alternate delegate

---
+++ belugapad/AppDelTest.h
@@ -0,0 +1,18 @@
+//
+//  AppDelTest.h
+//  belugapad
+//
+//  Created by gareth on 16/01/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelTest : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UIViewController *viewController;
+
+
+@end

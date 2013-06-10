fd38424fb488771d200b22c12b30a15dd8d2a920
Nick Cartwright <nick@aslant.io>
2013-06-10 17:00:50 +0100

    update news panel assets and move everything down

--- belugapad/BelugaNewsViewController.m
+++ belugapad/BelugaNewsViewController.m
@@ -23,24 +23,24 @@
     //self.view.frame = CGRectMake(0, 0, 699, 499);
     
     UIImageView *bg = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"/images/news-panel/News_panel.png"]] autorelease];
-    bg.center = CGPointMake(512, 364);
+    bg.center = CGPointMake(512, 410);
     [self.view addSubview:bg];
     
     UIButton *close = [UIButton buttonWithType:UIButtonTypeCustom];
-    close.frame = CGRectMake(910, 75, 48, 48);
+    close.frame = CGRectMake(910, 121, 48, 48);
     [close setImage:[UIImage imageNamed:@"/images/news-panel/News_close_button.png"] forState:UIControlStateNormal];
     [close addTarget:self action:@selector(closePanel:) forControlEvents:UIControlEventTouchDown];
     [self.view addSubview:close];
     
     UIButton *prev = [UIButton buttonWithType:UIButtonTypeCustom];
-    prev.frame = CGRectMake(103, 573, 131, 51);
+    prev.frame = CGRectMake(103, 619, 131, 51);
     [prev setImage:[UIImage imageNamed:@"/images/news-panel/previous_button.png"] forState:UIControlStateNormal];
     [prev setImage:[UIImage imageNamed:@"/images/news-panel/previous_button_disabled.png"] forState:UIControlStateDisabled];
     [prev addTarget:self action:@selector(prev:) forControlEvents:UIControlEventTouchDown];
     [self.view addSubview:prev];
     
     UIButton *next = [UIButton buttonWithType:UIButtonTypeCustom];
-    next.frame = CGRectMake(235, 573, 131, 51);
+    next.frame = CGRectMake(235, 619, 131, 51);
     [next setImage:[UIImage imageNamed:@"/images/news-panel/next_button.png"] forState:UIControlStateNormal];
     [next setImage:[UIImage imageNamed:@"/images/news-panel/next_button_disabled.png"] forState:UIControlStateDisabled];
     [next addTarget:self action:@selector(next:) forControlEvents:UIControlEventTouchDown];

3740c6240bf990901dddb63a3930766206d050b8
Nick Cartwright <nick@aslant.io>
2013-06-10 15:56:34 +0100

    update news-panel assets and add prev/next buttons

--- belugapad/BelugaNewsViewController.m
+++ belugapad/BelugaNewsViewController.m
@@ -27,16 +27,47 @@
     [self.view addSubview:bg];
     
     UIButton *close = [UIButton buttonWithType:UIButtonTypeCustom];
-    close.frame = CGRectMake(814, 130, 48, 48);
+    close.frame = CGRectMake(910, 75, 48, 48);
     [close setImage:[UIImage imageNamed:@"/images/news-panel/News_close_button.png"] forState:UIControlStateNormal];
     [close addTarget:self action:@selector(closePanel:) forControlEvents:UIControlEventTouchDown];
     [self.view addSubview:close];
+    
+    UIButton *prev = [UIButton buttonWithType:UIButtonTypeCustom];
+    prev.frame = CGRectMake(103, 573, 131, 51);
+    [prev setImage:[UIImage imageNamed:@"/images/news-panel/previous_button.png"] forState:UIControlStateNormal];
+    [prev setImage:[UIImage imageNamed:@"/images/news-panel/previous_button_disabled.png"] forState:UIControlStateDisabled];
+    [prev addTarget:self action:@selector(prev:) forControlEvents:UIControlEventTouchDown];
+    [self.view addSubview:prev];
+    
+    UIButton *next = [UIButton buttonWithType:UIButtonTypeCustom];
+    next.frame = CGRectMake(235, 573, 131, 51);
+    [next setImage:[UIImage imageNamed:@"/images/news-panel/next_button.png"] forState:UIControlStateNormal];
+    [next setImage:[UIImage imageNamed:@"/images/news-panel/next_button_disabled.png"] forState:UIControlStateDisabled];
+    [next addTarget:self action:@selector(next:) forControlEvents:UIControlEventTouchDown];
+    [self.view addSubview:next];
+    
+    prev.enabled = NO;
+    next.enabled = NO;
+    
+    [close registerForDragAndLog];
+    [prev registerForDragAndLog];
+    [next registerForDragAndLog];
 }
 
 -(void)closePanel:(id)button
 {
     [self.view removeFromSuperview];
     if (self.delegate) [self.delegate newPanelWasClosed];
+}
+
+-(void)prev:(id)button
+{
+    
+}
+
+-(void)next:(id)button
+{
+    
 }
 
 -(void)dealloc

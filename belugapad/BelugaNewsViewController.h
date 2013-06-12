0c9864d3855e8ad238dd49e02422e8a6143b4e86
Nick Cartwright <nick@aslant.io>
2013-06-12 18:31:26 +0100

    have links from news items open in safari not the uiwebview

--- belugapad/BelugaNewsViewController.h
+++ belugapad/BelugaNewsViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 @protocol BelugaNewsDelegate;
 
-@interface BelugaNewsViewController : UIViewController
+@interface BelugaNewsViewController : UIViewController <UIWebViewDelegate>
 
 @property (retain) id<BelugaNewsDelegate> delegate;
 
